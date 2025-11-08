6a633b6aa61750093fc6ea527427f3f6d116a92d
BIT504 <cynergykinghuaki@myop.ac.nz>
2025-11-09 10:29:47 +1300

    Added a String final message

--- Hello/helloworld/HelloWorld.java
+++ Hello/helloworld/HelloWorld.java
@@ -4,8 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		
+		public static final String MESSAGE = "I AM AN ALIEN!";
 		System.out.println("Hello World");
+		System.out.println(MESSAGE);
 
 	}
 
