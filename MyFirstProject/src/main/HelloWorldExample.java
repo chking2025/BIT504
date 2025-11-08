b616240e104973d585a958810ba96a044acd3567
BIT504 <cynergykinghuaki@myop.ac.nz>
2025-11-09 08:49:49 +1300

    Initial upload of the project

---
+++ MyFirstProject/src/main/HelloWorldExample.java
@@ -0,0 +1,11 @@
+package main;
+
+public class HelloWorldExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		System.out.println("My name is Cynergy and this is my first Java Program");
+	}
+
+}
